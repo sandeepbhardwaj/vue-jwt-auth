b43cf90c615d6274c74d6e49556a5ddcf05f7704
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2020-02-10 20:59:22 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <a href class="navbar-brand" @click.prevent>bezKoder</a>
+      <a href class="navbar-brand" @click.prevent>SpringBootJWT</a>
       <div class="navbar-nav mr-auto">
         <li class="nav-item">
           <router-link to="/home" class="nav-link">
